de76979595d1902a1bcbe528b604f97c883fb344
Justin900308 <wow3987@gmail.com>
2025-08-29 01:05:08 -0700

    Add files via upload

--- server_src/server/util/global_constants.py
+++ server_src/server/util/global_constants.py
@@ -7,21 +7,31 @@ data_length = 20
 duration = 120.0
 ## controller gains and limits
 ################### Position controller
-P_kp = 0.7
+P_kp = 1.0
 P_kd = 0.0
-v_max = 0.5
+v_max = 0.25
 
 ## room dimensions
-x_max = 1
-x_min = -1
-y_max = 1
-y_min = -0.1
+x_max = 0.9
+x_min = -0.6
+y_max = 0.9
+y_min = -0.6
 z_max = 1.5
-z_min = 0.1
+z_min = 0.0
 
-## static obstacles
-R_obs = 0.5
-X_obs = np.array([0.5, 0.5, 0.5])
+
+## quad
+R_quad = 0.2
+
+## static obs
+R_obs = np.array([0.2,0.2])
+num_obs = len(R_obs)
+# R_obs = 0.3
+X_obs = np.array([[0.5, 0.7, 0.4],
+                  [0.3, .0, .5]])
+
+
+
 
 ## CBF parameters
 a = 1.2
--- server_src/server/util/util_fcns.py
+++ server_src/server/util/util_fcns.py
@@ -5,7 +5,9 @@ import threading
 import cvxpy as cp
 from numpy import linalg as LA
 from scipy.spatial.transform import Rotation as rot
-from .global_constants import x_max,x_min,y_max,y_min,z_max,z_min,R_obs,v_max
+from .global_constants import x_max, x_min, y_max, y_min, z_max, z_min, R_obs, v_max
+
+
 def threaded(fn):
     def wrapper(*args, **kwargs):
         thread = Thread(target=fn, args=args, kwargs=kwargs)
@@ -36,25 +38,28 @@ def q2e(q: np.ndarray):
     e = r.as_euler("xyz", degrees=True)
     return e
 
+
 def e2q(e: np.ndarray):
     r = rot.from_euler("xyz", e, degrees=True)
     q = r.as_quat()
     return q
 
 
-
-def cbf(X_t, v_des,X_obs,dt):
+def cbf(X_t, v_des, obs_list: list, obs_r: list, dt):
     u = cp.Variable(3)
     a = 1.2
     f = cp.norm(u - v_des, 2)
-    h_t = LA.norm(X_t - X_obs,2)**2 - R_obs ** 2
-    grad_h_i = 2 * (X_t - X_obs)
+    num_obs = len(obs_list)
     f_t = u
-    constraint = [2 * grad_h_i.T @ f_t >= -a * h_t]
+    constraint = []
+    for i in range(num_obs):
+        obs_i = obs_list[i]
+        h_i = LA.norm(X_t - obs_i, 2) ** 2 - obs_r[i] ** 2
+        grad_h_i = 2 * (X_t - obs_i)
+        constraint.append(2 * grad_h_i.T @ f_t >= -a * h_i)
     constraint.append(X_t[2] + u[2] * dt >= -0.001)
-    constraint.append(cp.norm(u,2)<=v_max)
+    constraint.append(cp.norm(u, 2) <= v_max)
     problem = cp.Problem(cp.Minimize(f), constraint)
     problem.solve(solver=cp.CLARABEL)
     u_val = u.value
     return u_val
-
